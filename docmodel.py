38b587d6ef272677e8023239e533a64c8bd8d16c
nstbayless <nstbayless@gmail.com>
2018-06-07 01:01:30 -0700

    decently workable parser

--- docmodel.py
+++ docmodel.py
@@ -4,94 +4,164 @@ import xml.etree.ElementTree as ET
 import glob
 import os
 import re
+import markdown
+from constants import *
 
-reLines = re.compile("//.*?$|/\*.*?\*|^\s*([a-zA-Z_0-9])+\s*=([^\n]*//([^\n]*))?\n", re.MULTILINE)
+reLines = re.compile("//.*?$|/\*.*?\*/|^\s*([a-zA-Z_0-9]+)\s*=(.*//(.*)$)?", re.MULTILINE)
 
 class VarModel:
-    name = ""
-    docText = ""
+    def __init__(self):
+        self.name = ""
+        self.docText = ""
+    
+    def __repr__(self):
+        return self.name + ": " + self.docText
 
 class ObjectModel:
-    name = "<undefined>"
-    spriteName = ""
-    parentName = ""
-    docText = ""
-    ccv = [] # creation code variables.
-    v = [] # non-creation code variables
+    def __init__(self, dm):
+        self.name = "<undefined>"
+        self.spriteName = ""
+        self.parentName = ""
+        self.docText = ""
+        self.vars = [] # variables
+        self._varnames = []
+        self._docmodel = dm
+    
+    def __reprvars__(self):
+        tr = ""
+        for var in self.vars:
+            tr += "\n  " + var.__repr__()
+        if len(self.parentName) > 0:
+            for obj in self._docmodel.objects:
+                if obj.name == self.parentName:
+                    tr += "\n\n -- inherited from " + obj.name + " --"
+                    tr += obj.__reprvars__()
+        return tr
+    
+    def __repr__(self):
+        tr =  self.name + ": " + \
+            "\n  sprite: " + self.spriteName + \
+            (("\n  parent: " + self.parentName) if len(self.parentName) > 0 else "")
+        if len(self.docText) > 0:
+            tr += "\n\n" + self.docText + "\n\n"
+        tr += "\n\n -- members --"
+        tr += self.__reprvars__()
+        return tr
 
 class DocModel:
-    objects = []
-    scripts = []
+    def __init__(self):
+        self.objects = []
+        self.scripts = []
+        
+    def __repr__(self):
+        tr = "objects: "
+        for obj in self.objects:
+            tr += "\n  " + obj.__repr__()
+        return tr
+    
+    def parseDocText(self, lines):
+        text = ""
+        for l in lines:
+            text += l[1] + "\n"
+        return markdown.markdown(text.strip())
     
     def parseObject(self, objectFile):
         tree = ET.parse(objectFile)
         root = tree.getroot()
-        if (root.tag != "object")
+        if root.tag != "object":
             root = root[0]
         assert(root.tag == "object")
-        obj = ObjectModel()
-        obj.name = os.path.splitext(os.path.basename(objectFile))[0]
+        obj = ObjectModel(self)
+        obj.name = os.path.splitext(os.path.splitext(os.path.basename(objectFile))[0])[0]
         eltSpriteName = root.find("spriteName")
         obj.spriteName = eltSpriteName.text
         eltParentName = root.find("parentName")
         obj.parentName = eltParentName.text
-        
         # event parse
         eltEvents = root.find("events")
+        lines = []
         for eltEvent in eltEvents:
             if "eventtype" in eltEvent.attrib and "enumb" in eltEvent.attrib:
-                if eltEvent.attrib["eventtype"] == "0" and eltEvent.attrib["enumb"] == 0:
+                if eltEvent.attrib["eventtype"] == "0" and eltEvent.attrib["enumb"] == "0":
                     # collect list of comments, blank lines, and variable declarations
-                    lines = []
-                    LT_COMMENT = 0
-                    LT_POSTCOMMENT = 1
-                    LT_VAR = 2
-                    LT_JUNK = 3
-                    LT_BLANK = 4
-                    LT_SEP = 5
-                    for action in eltEvent.findall("action"):
-                        actID = -1
-                        actKind = -1
-                        for id in eltEvent.findall("id"):
-                            actID = id
-                        for kind in eltEvent.findall("kind"):
-                            actKind = kind
-                        if actID == 603 and actKind == 7: # code block
-                            argumentElt = eltEvents.find("arguments")
-                            for argument in argumentElt:
-                                text = argument.text
-                                prev = 0
-                                for m in reLines.finditer(text):
-                                    start = m.start()
-                                    
-                                    # check for junk in between matches
-                                    prevLine = text[prev:start]
-                                    if prevLine.strip() != "":
-                                        lines.append((LT_JUNK, prevLine))
-                                    elif prevLine.count("\n") > 1:
-                                        lines.append((LT_BLANK, prevLine))
-                                    
-                                    # determine line type
-                                    line = m.group()
-                                    if line[0:2] == "//":
-                                        lines.append(LT_COMMENT, line[2:])
-                                    elif line[0:2] == "/*":
-                                        lines.append(LT_COMMENT, line[2:-2])
-                                    else:
-                                        lines.append((LT_VAR, line.group(1)))
-                                        if len(line.group(3)) != "":
-                                            lines.append((LT_POSTCOMMENT, line.group(3)))
-                                    prev = start
-                        lines.append(LT_SEP, "---- action separator ----")
-                        print(lines)
-                            
+                    lines = self._collectLines(eltEvent)
+        
+        docTextParsed = False # has the doctext for the object been read?
+        for i in range(len(lines)):
+            l = lines[i]
+            lType = l[0]
+            lText = l[1]
+            
+            if lType == LT_VAR or lType == LT_JUNK or lType == LT_SEP:
+                proposedDocIDX = i # index of last line of doctext
+                if lType == LT_VAR:
+                    var = lText
+                    if i > 0:
+                        if lines[i-1][0] == LT_COMMENT:
+                            proposedDocIDX = i-1
+                            var.docText = self.parseDocText([lines[i-1]])
+                    if len(lines) > i + 1:
+                        if lines[i+1][0] == LT_POSTCOMMENT:
+                            proposedDocIDX = i
+                            var.docText = self.parseDocText([lines[i+1]])
+                    if var.name not in obj._varnames:
+                        obj._varnames.append(var.name)
+                        obj.vars.append(var)
+                if not docTextParsed:
+                    docTextParsed = True
+                    obj.docText = self.parseDocText(lines[0:proposedDocIDX])
+        
         self.objects.append(obj)
     
-    def parseDocModel(self, projectpath):
-        objectFiles = glob(os.path.join(projectpath, "objects/*.object.gmx"))
-        scriptFiles = glob(os.path.join(projectpath, "scripts/*.gml"))
+    def parseProject(self, projectpath):
+        objectFiles = glob.glob(os.path.join(projectpath, "objects/*.object.gmx"))
+        scriptFiles = glob.glob(os.path.join(projectpath, "scripts/*.gml"))
         
         # parse objects
         for objectFile in objectFiles:
-            parseObject(objectFile)
-        
+            self.parseObject(objectFile)
+
+    def _collectLines(self, eltEvent):
+        lines = []
+        for action in eltEvent.findall("action"):
+            actID = -1
+            actKind = -1
+            for id in action.findall("id"):
+                actID = id.text
+            for kind in action.findall("kind"):
+                actKind = kind.text
+            if actID == "603" and actKind == "7": # code block
+                argumentElt = action.find("arguments")
+                for argument in argumentElt:
+                    stringElt = argument.find("string")
+                    text = stringElt.text
+                    prev = 0
+                    for m in reLines.finditer(text):
+                        start = m.start()
+                        
+                        # check for junk in between matches
+                        prevLine = text[prev:start]
+                        if prevLine.strip() != "":
+                            lines.append((LT_JUNK, prevLine))
+                        elif prevLine.count("\n") > 1:
+                            lines.append((LT_BLANK, prevLine))
+                        
+                        # determine line type
+                        line = m.group()
+                        if line[0:2] == "//":
+                            while line.startswith('/'):
+                                line = line[1:]
+                            lines.append((LT_COMMENT, line))
+                        elif line[0:2] == "/*":
+                            while line.startswith("*"):
+                                line = line[1:]
+                            lines.append((LT_COMMENT, line[0:-2]))
+                        else:
+                            var = VarModel()
+                            var.name = m.group(1)
+                            lines.append((LT_VAR, (var)))
+                            if m.group(3) != None:
+                                lines.append((LT_POSTCOMMENT, m.group(3)))
+                        prev = m.end()
+            lines.append((LT_SEP, "---- action separator ----"))
+        return lines
--- gendoc.py
+++ gendoc.py
@@ -14,36 +14,37 @@ def isValidProjectDirectory(path):
     if os.path.isfile(path):
         return False
     if os.path.exists(path):
-        paths = glob.glob(os.path.join(path, "*.project.gmx")
+        paths = glob.glob(os.path.join(path, "*.project.gmx"))
         if len(paths) == 1:
-            return os.path.split(paths[0])[1]
+            return os.path.split(paths[0])[0]
         print("Error: expected one .project.gmx file in given directory")
     return False
 
 def main():
-    if len(sys.argv) == 0:
-        print_usage()
+    if len(sys.argv) <= 1:
+        printUsage()
         return -1
         
     # input project
-    path = os.path(sys.argv[1])
+    path = sys.argv[1]
     projectpath = isValidProjectDirectory(path)
     
-    if project == False:
+    if projectpath == False:
         print("Error: invalid project directory")
         return -1
     
     # output directory
-    buildpath = path.join(projectpath, "docs/build/")
-    if len(sys.argv > 2):
-        os.path(sys.argv[2]
+    buildpath = os.path.join(projectpath, "docs/build/")
+    if len(sys.argv) > 2:
+        sys.argv[2]
     
     # create output directory
     if not os.path.isdir(buildpath):
         os.makedirs(buildpath)
         
-    docmodel = docmodel.generateDocModel(projectpath)
-    print(docmodel)
+    dcm = docmodel.DocModel()
+    dcm.parseProject(projectpath)
+    print(dcm)
     return 0
     
 errcode = main()
